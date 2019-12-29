5f7467267062e52f5ed17d85f2fb60f21e404f62
cdutten <cdutten@medallia.com>
2019-12-29 16:23:12 -0300

    Add configuration for fonts and styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from 'lodash';
+import './style.scss';
+import Logo from './laulogo.png';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  
+
+  const myLogo = new Image();
+  myLogo.src = Logo;
+  element.appendChild(myLogo);
 
   return element;
 }
