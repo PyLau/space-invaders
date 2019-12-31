18766d1f88bcb60c7abc2677473930fd53fe800e
cdutten <cdutten@medallia.com>
2019-12-31 05:57:26 -0300

    add webpack dev server and pug config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
   },
   module: {
     rules: [
@@ -28,6 +34,16 @@ module.exports = {
           'file-loader',
         ],
       },
+      {
+        test: /\.pug$/,
+        use: 'pug-loader',
+        exclude: /node_modules/
+      }
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.pug'
+    }),
+  ]
 };
